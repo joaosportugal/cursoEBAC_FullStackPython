03fd7fcfbf9fb2d127ab7f7c00eec35e36004fa5
Joao Portugal <joaosportugal@hotmail.com>
2023-04-04 16:57:42 -0300

    criadas as funcoes multiplicar e saudar

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(12, 5000));
+console.log(saudar('tutor'));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2
+}
+
+function saudar(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicar(12, 5000));
+console.log(saudar('tutor'));
